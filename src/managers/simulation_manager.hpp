7f4057f38e0803a5796a110f1d101278ee96a86e
bfeld <dev@bfeld.eu>
2021-03-01 15:08:11 +0100

    Test Box2D integration

--- src/managers/simulation_manager.hpp
+++ src/managers/simulation_manager.hpp
@@ -4,6 +4,7 @@
 #include <chrono>
 #include <memory>
 #include <thread>
+#include <box2d/box2d.h>
 #include <concurrentqueue/concurrentqueue.h>
 
 class SimulationManager
@@ -14,6 +15,9 @@ class SimulationManager
         void init()
         {
             Thread_ = std::thread(&SimulationManager::run, this);
+            World_ = new b2World({0.0f, 0.0f});
+            World2_ = new b2World({0.0f, 0.0f});
+            std::cout << "Created Worlds" << std::endl;
         }
 
     private:
@@ -22,12 +26,17 @@ class SimulationManager
         {
             while (!ExitSimulation_)
             {
-                std::cout << "[Thread] Simulation running" << std::endl;
-                std::this_thread::sleep_for(std::chrono::seconds(1));
+                // std::cout << "[Thread] Simulation running" << std::endl;
+                World_->Step(1.0f/60.0f, 8, 3);
+                World2_->Step(1.0f/60.0f, 8, 3);
+                // World_->ShiftOrigin({20.0f, 10.0f});
+                std::this_thread::sleep_for(std::chrono::milliseconds(16));
             }
             std::cout << "[Thread] Simulation exited." << std::endl;
         }
 
+        b2World*    World_;
+        b2World*    World2_;
         std::thread Thread_;
         bool        ExitSimulation_{false};
 };
