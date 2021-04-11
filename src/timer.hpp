698ebe98e6f8452e49d4fb0e27f41d812ea83ce5
bfeld <dev@bfeld.eu>
2021-04-11 19:46:51 +0200

    Add some convenience methods to timer class

--- src/timer.hpp
+++ src/timer.hpp
@@ -10,12 +10,21 @@ class Timer
 {
     public:
         Timer() {this->start();}
-        void    start()     {Start = HiResClock::now();}
-        void    stop()      {Stop = HiResClock::now();}
-        void    restart()   {stop(); start();}
-        double  elapsed()   {return duration<double>(Stop-Start).count();}
-        double  split()     {return duration<double>(HiResClock::now() - Start).count();}
-        double  time()      {return duration<double>(HiResClock::now() - Start).count();}
+        void    start()        {Start = HiResClock::now();}
+        void    stop()         {Stop = HiResClock::now();}
+        void    restart()      {stop(); start();}
+        double  elapsed()      {return duration<double>(Stop-Start).count();}
+        double  elapsed_s()    {return duration<double>(Stop-Start).count();}
+        double  elapsed_ms()   {return duration<double>(Stop-Start).count()*1000.0;}
+        double  elapsed_us()   {return duration<double>(Stop-Start).count()*1.0e6;}
+        double  split()        {return duration<double>(HiResClock::now() - Start).count();}
+        double  split_s()      {return duration<double>(HiResClock::now() - Start).count();}
+        double  split_ms()     {return duration<double>(HiResClock::now() - Start).count()*1000.0;}
+        double  split_us()     {return duration<double>(HiResClock::now() - Start).count()*1.0e6;}
+        double  time()         {return duration<double>(HiResClock::now() - Start).count();}
+        double  time_s()       {return duration<double>(HiResClock::now() - Start).count();}
+        double  time_ms()      {return duration<double>(HiResClock::now() - Start).count()*1000.0;}
+        double  time_us()      {return duration<double>(HiResClock::now() - Start).count()*1.0e6;}
 
     private:
         
