80e1501aeee7144f914ce4b7876c237657408801
bfeld <dev@bfeld.eu>
2021-07-24 10:48:22 +0200

    Update sim timer with convenience methods

--- src/sim_timer.cpp
+++ src/sim_timer.cpp
@@ -1,34 +1,48 @@
 #include "sim_timer.hpp"
 
+void SimTimer::fromStamp(const std::string& _s)
+{
+    auto p = _s.find(':');
+    if (p != std::string::npos)
+    {
+        Years_ = std::stoul(_s.substr(0, p), nullptr);
+        Seconds_ = std::stod(_s.substr(p+1, std::string::npos));
+    }
+    else
+    {
+        // error
+    }
+}
+
 void SimTimer::inc(const double& _Seconds)
 {
     constexpr double S_PER_Y = 365.0*24.0*60.0*60.0;
-    if (Active)
+    if (Active_)
     {
-        Seconds += _Seconds;
-        while (Seconds >= S_PER_Y)
+        Seconds_ += _Seconds;
+        while (Seconds_ >= S_PER_Y)
         {
-            Seconds -= S_PER_Y;
-            ++Years;
+            Seconds_ -= S_PER_Y;
+            ++Years_;
         }
     }
 }
 
 void SimTimer::start()
 {
-    Active = true;
-    Seconds = 0.0;
-    Years = 0u;
+    Active_ = true;
+    Seconds_ = 0.0;
+    Years_ = 0u;
 }
 
 void SimTimer::stop()
 {
-    Active = false;
+    Active_ = false;
 }
 
 void SimTimer::toggle()
 {
-    if (Active)
+    if (Active_)
     {
         this->stop();
     }
--- src/sim_timer.hpp
+++ src/sim_timer.hpp
@@ -1,6 +1,7 @@
 #ifndef SIM_TIMER_HPP
 #define SIM_TIMER_HPP
 
+#include <cmath>
 #include <cstdint>
 #include <string>
 
@@ -10,11 +11,16 @@ class SimTimer
     public:
         
         std::uint32_t getYears() const;
+        std::uint32_t getDaysFraction() const;
+        std::uint32_t getHoursFraction() const;
+        std::uint32_t getMinutesFraction() const;
+        std::uint32_t getSecondsFraction() const;
         double        getSeconds() const;
         std::string   toStamp() const;
         
         bool isActive() const;
-        
+
+        void fromStamp(const std::string& _s);
         void inc(const double& _Seconds);
         void start();
         void stop();
@@ -22,29 +28,57 @@ class SimTimer
 
     private:
         
-        bool            Active{false};
-        double          Seconds{0.0};
-        std::uint32_t   Years{0u};
+        bool            Active_{false};
+        double          Seconds_{0.0};
+        std::uint32_t   Years_{0u};
 };
 
 inline std::uint32_t SimTimer::getYears() const
 {
-    return Years;
+    return Years_;
+}
+
+inline std::uint32_t SimTimer::getSecondsFraction() const
+{
+    constexpr double M_PER_S = 1.0/60.0;
+    constexpr double S_PER_M = 60.0;
+    return static_cast<uint32_t>(((Seconds_ * M_PER_S) - std::floor(Seconds_ * M_PER_S)) * S_PER_M);
+}
+
+inline std::uint32_t SimTimer::getMinutesFraction() const
+{
+    constexpr double M_PER_H = 60.0;
+    constexpr double H_PER_S = 1.0/(60.0*M_PER_H);
+    return static_cast<uint32_t>(((Seconds_ * H_PER_S) - std::floor(Seconds_ * H_PER_S)) * M_PER_H);
+}
+
+inline std::uint32_t SimTimer::getHoursFraction() const
+{
+    constexpr double H_PER_D = 24.0;
+    constexpr double D_PER_S = 1.0/(60.0*60.0*H_PER_D);
+    return static_cast<uint32_t>(((Seconds_ * D_PER_S) - std::floor(Seconds_ * D_PER_S)) * H_PER_D);
+}
+
+inline std::uint32_t SimTimer::getDaysFraction() const
+{
+    constexpr double D_PER_Y = 365.0;
+    constexpr double Y_PER_S = 1.0/(60.0*60.0*24.0*D_PER_Y);
+    return static_cast<uint32_t>(((Seconds_ * Y_PER_S) - std::floor(Seconds_ * Y_PER_S)) * D_PER_Y);
 }
 
 inline double SimTimer::getSeconds() const
 {
-    return Seconds;
+    return Seconds_;
 }
 
 inline std::string SimTimer::toStamp() const
 {
-    return std::to_string(Years) + ":" + std::to_string(Seconds);
+    return std::to_string(Years_) + ":" + std::to_string(Seconds_);
 }
 
 inline bool SimTimer::isActive() const
 {
-    return Active;
+    return Active_;
 }
 
 #endif // SIM_TIMER_HPP
